50fd9b4127980c9984d20b040626121dd58db600
Diarintsoa1402 <diarintsoa1402@gmail.com>
2025-07-03 12:09:00 +0300

    Mise à jour backend image

--- models/projectModel.js
+++ models/projectModel.js
@@ -2,23 +2,25 @@ const pool = require('../config/db');
 
 
 const ProjectModel = {
-  async getAll() {
-    const result = await pool.query('SELECT * FROM projects');
-    return result.rows;
-  },
+async getAll() {
+  const result = await pool.query('SELECT * FROM projects');
+  return result.rows;
+}
+,
 
   async getById(id) {
     const result = await pool.query('SELECT * FROM projects WHERE id = $1', [id]);
     return result.rows[0];
   },
 
-  async create(data) {
-    const { title, description, image, link, tags } = data;
-    await pool.query(
-      'INSERT INTO projects (title, description, image, link, tags) VALUES ($1, $2, $3, $4, $5)',
-      [title, description, image, link, tags]
-    );
-  },
+ async create(data) {
+  const { title, description, image, link, tags } = data;
+  await pool.query(
+    'INSERT INTO projects (title, description, image, link, tags) VALUES ($1, $2, $3, $4, $5)',
+    [title, description, image, link, tags]
+  );
+}
+,
 
   async update(id, data) {
     const { title, description, image, link, tags } = data;
